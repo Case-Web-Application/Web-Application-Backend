d205e54c3fa25e2e4ccfb2dca654fc8bb9fc15ba
Mikhail <majorov-2004@list.ru>
2023-05-26 18:17:47 +0500

    modified:   main_case/testov/models.py

--- main_case/testov/models.py
+++ main_case/testov/models.py
@@ -1,13 +1,13 @@
-
 from django.db import models
 from datetime import date
+
 class User(models.Model):
     login = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     age = models.IntegerField()
-    
+
     def __str__(self):
         return f"{self.id}) {self.login}"
     
