98677461d7f489616d52c8dcf94a4cd2657978c3
Mikhail <majorov-2004@list.ru>
2023-06-02 21:04:16 +0500

    Made authorization

--- main_case/testov/api.py
+++ main_case/testov/api.py
@@ -1,6 +1,11 @@
 from ninja import NinjaAPI, Schema, Router
 from .models import *
-
+from ninja.security import django_auth
+import jwt
+import base64
+from base64 import b64decode
+from jwt import decode
+from ninja.security import HttpBearer
 router = Router()
 
 class UsersIn(Schema):
@@ -10,6 +15,12 @@ class UsersIn(Schema):
     last_name: str
     age: int
 
+class AuthBearer(HttpBearer):
+    def authenticate(self, request, token):
+        key = "super-s3cr3t--pass$word"
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEiLCJsb2dpbiI6Ik1pa2UyMDA0IiwicGFzc3dvcmQiOiIxMjM0NTY3OCIsImZpcnN0X25hbWUiOiJNaWtoYWlsIiwibGFzdF9uYW1lIjoiTWFqb3JvdiIsImFnZSI6IjE4In0.lPCLFYeuPY8xC4AmULCBHqw_3ZJ_T-WYn67UWc8YcpA"
+        return jwt.decode(token, key, algorithms="HS256")["id"]
+
 #http://127.0.0.1:8000/api/v1/createuser
 @router.post("/createuser")
 def create_users(request, payload: UsersIn):
@@ -17,9 +28,10 @@ def create_users(request, payload: UsersIn):
     return {"id": employee.id}
 
 #http://127.0.0.1:8000/api/v1/getusers
-@router.get("/getusers")
+@router.get("/getusers", auth=AuthBearer())
 def get_users(request):
-    all_users = User.objects.all()
+    id_user = request.auth
+    all_users = User.objects.filter(id = id_user)
     response = []
     for x in all_users:
         response.append({
